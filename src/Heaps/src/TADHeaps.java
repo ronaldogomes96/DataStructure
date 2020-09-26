ba22ecef61b55c4f0222d9229ea9510d85d5c273
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-09-26 20:26:55 -0300

    Maximo Heaps

---
+++ src/Heaps/src/TADHeaps.java
@@ -0,0 +1,12 @@
+public interface TADHeaps<T> {
+
+    public void insereValor(T valor);
+    public T deletePosicao(int posicao);
+    public int posicaoPai(int i);
+    public int posicaoFilhoDireito(int i);
+    public int posicaoFilhoEsquerdo(int i);
+    public boolean heapVazia();
+    public boolean heapCheia();
+    public void imprimeHeap();
+
+}
